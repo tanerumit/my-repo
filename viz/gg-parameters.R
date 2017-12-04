38f98dab4c1f26c7e2887e245a6853b6789d336b
tanerumit <tanerumit@gmail.com>
2017-12-04 11:22:32 -0500

    routine maintainance

--- viz/gg-parameters.R
+++ viz/gg-parameters.R
@@ -2,35 +2,44 @@
 ###### SIZING 
 
 # One figure per powerpoint slide:
-#png(file="****.png",width=600,height=400, res=80) ///ONE FIGURE PER SLIDE
+#png(file="****.png", width=600, height=400, res=80)
 
+theme_set(theme_light(base_size = 10))
 
+#----------------------------------------
+# CLIMATE RESPONSE SURFACE PLOTS THEME
+#----------------------------------------
+
+theme_climsurface <-theme_update(
+  plot.title = element_text(face = "bold")
+)
+
+climsurface_axis_labels <- list(
+  temp = expression("Temperature change (" * degree * C *")"),
+  prcp = "Precipitation change (%)"
+)
 
 #----------------------------------------
 # GENERAL THEME
-# - we'll use this for most of our charts and build on it when we need to
 #----------------------------------------
 
-theme.porttheme <-
-  theme(text = element_text(family = "Gill Sans", color = "#444444")) +
-  theme(plot.title = element_text(size = 24)) +
-  theme(plot.subtitle = element_text(size = 18)) +
-  theme(axis.title = element_text(size = 14)) +
-  theme(axis.title.y = element_text(angle = 0, vjust = .5, margin = margin(r = 15))) +
-  theme(axis.text = element_text(size = 10)) +
-  theme(axis.title.x = element_text(margin = margin(t = 20))) +
-  theme(legend.title = element_blank()) +
-  theme(panel.grid.major.x = element_line(color = "#F3F3F3")) +
-  theme(panel.grid.major.y = element_blank()) +
-  theme(panel.grid.minor   = element_blank()) +
-  theme(legend.position    = "none")
-
+theme.porttheme <- theme(
+  text               = element_text(family = "Gill Sans", color = "#444444"),
+  plot.title         = element_text(size = 24),
+  plot.subtitle      = element_text(size = 18),
+  axis.title         = element_text(size = 14),
+  axis.title.y       = element_text(angle = 0, vjust = .5, margin = margin(r = 15)),
+  axis.text          = element_text(size = 10),
+  axis.title.x       = element_text(margin = margin(t = 20)),
+  legend.title       = element_blank(),
+  panel.grid.major.x = element_line(color = "#F3F3F3"),
+  panel.grid.major.y = element_blank(),
+  panel.grid.minor   = element_blank(),
+  legend.position    = "none"
+)
 
 #------------------------------------
 # THEME FOR 'WIDE' BAR CHARTS
-# - there are several bar charts that
-#   are very wide, and need some
-#   special formatting
 #------------------------------------
 
 theme.widebar <-
@@ -42,12 +51,8 @@ theme.widebar <-
   theme(legend.position = c(.9,.55)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .4))
 
-
 #-----------------------------------
-# THEME FOR 'WIDE' BAR CHARTS
-#  - we'll use this for small multiple
-#    charts.  these also have some
-#    special formatting requirements
+# THEME FOR 'WIDE' BAR CHARTS (2)
 #------------------------------------
 
 theme.smallmult <-
